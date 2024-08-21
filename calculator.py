be3cd1db16de5f8e05246ac71e6f1b302a90959c
Anushree G D <anushreegdanu@gmail.com>
2024-08-21 08:16:40 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,28 +1,45 @@
-from functions import *
+def addition(num1, num2):
+    result = num1 + num2
+    print("{0} + {1} = {2}".format(num1, num2, result))
+
+def subtraction(num1, num2):
+    result = num1 - num2
+    print("{0} - {1} = {2}".format(num1, num2, result))
+
+def multiplication(num1, num2):
+    result = num1 * num2
+    print("{0} * {1} = {2}".format(num1, num2, result))
+
+def division(num1, num2):
+    if num2 == 0.0:
+        print("Can't divide by zero")
+    else:
+        result = num1 / num2
+        print("{0} / {1} = {2}".format(num1, num2, result))
 
 while True:
-    print("choose any option")
+    print("Choose any option:")
     print("1 Addition")
     print("2 Subtraction")
     print("3 Multiplication")
-    print("4 division")
-    print("press Q key to exit")
+    print("4 Division")
+    print("Press Q key to exit")
 
-    choice = input("enter your choice : ")
+    choice = input("Enter your choice: ")
 
     if choice == 'q' or choice == 'Q':
         break
 
-    num1 = float(input("enter the number 1 : "))
-    num2 = float(input("enter the number 2 : "))
+    num1 = float(input("Enter number 1: "))
+    num2 = float(input("Enter number 2: "))
 
     if choice == '1':
-        addition(num1,num2)
+        addition(num1, num2)
     elif choice == '2':
-        subtraction(num1,num2)
+        subtraction(num1, num2)
     elif choice == '3':
-        multiplication(num1,num2)
+        multiplication(num1, num2)
     elif choice == '4':
-        division(num1,num2)
+        division(num1, num2)
     else:
-        print("Invalid choice") 
+        print("Invalid choice")
